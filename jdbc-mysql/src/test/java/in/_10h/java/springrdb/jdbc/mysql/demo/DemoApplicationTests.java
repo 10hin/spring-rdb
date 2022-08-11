c0004e2edee6d60f7df0fca2fb74002be43c3e3b
10hin <10hin@outlook.jp>
2022-08-12 00:38:21 +0900

    add wireframe

---
+++ jdbc-mysql/src/test/java/in/_10h/java/springrdb/jdbc/mysql/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in._10h.java.springrdb.jdbc.mysql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ r2dbc-mysql/src/main/java/in/_10h/java/springrdb/r2dbc/postgresql/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package in._10h.java.springrdb.r2dbc.postgresql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
