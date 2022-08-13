ae0fd8cc4b6fa7f18f3996be06a17bb54079cf95
10hin <10hin@outlook.jp>
2022-08-14 00:16:33 +0900

    fix configs

--- r2dbc-postgresql/src/test/java/in/_10h/java/springrdb/r2dbc/postgresql/demo/DemoApplicationTests.java
+++ r2dbc-postgresql/src/test/java/in/_10h/java/springrdb/r2dbc/postgresql/demo/DemoApplicationTests.java
@@ -2,8 +2,12 @@ package in._10h.java.springrdb.r2dbc.postgresql.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootTest
+@SpringBootTest(properties={
+	"spring.profiles.active[0]=default",
+	"spring.profiles.include[0]=test",
+})
 class DemoApplicationTests {
 
 	@Test
