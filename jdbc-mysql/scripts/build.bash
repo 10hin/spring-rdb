33cd5bb0382c5489a20f0baa8aa4d1273caf5de1
10hin <10hin@outlook.jp>
2022-08-15 23:48:50 +0900

    add rm option to build script

--- jdbc-mysql/scripts/build.bash
+++ jdbc-mysql/scripts/build.bash
@@ -6,6 +6,7 @@ cd "$(dirname "$0")/.." \
 nerdctl run \
   -v "${HOME}/.docker/config.json":/kaniko/.docker/config.json:ro \
   -v "${PWD}":/workspace \
+  --rm \
   gcr.io/kaniko-project/executor:latest \
   --dockerfile=/workspace/package/main/Dockerfile \
   --context=dir:///workspace/ \
